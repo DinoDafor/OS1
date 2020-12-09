c7c628220cea8eb00d3d048ae063fd2a21d72e66
Mitin <dinodafor@mail.ru>
2020-12-09 23:19:41 +0300

    Changed naming, i still have problem with multithreading...

--- main.c
+++ main.c
@@ -30,8 +30,7 @@
 
 Используя stap построить графики системных характеристик.
  */
-//vancA=197;B=0xA35E1090;C=mmap;D=78;E=72;F=block;G=10;H=random;I=35;J=min;K=sem
-//mitin A=128;B=0x74594352;C=malloc;D=55;E=144;F=block;G=65;H=seq;I=48;J=avg;K=cv
+// A=128;B=0x74594352;C=malloc;D=55;E=144;F=block;G=65;H=seq;I=48;J=avg;K=cv
 
 #define A 128
 #define D 55
@@ -39,188 +38,185 @@
 #define G 65
 #define I 48
 
-//todo свои значения и найминг
-const int filenumbers = 3;
+
+const int number_of_files = 3;
 
 pthread_mutex_t mut1;
 pthread_mutex_t mut2;
 pthread_mutex_t mut3;
 
 
-int loop = 1;
+int loop_flag = 1;
 
 typedef struct {
-    char* address;
+    char *address;
     size_t size;
-    FILE* file;
-}thread_info;
+    FILE *file;
+} info_for_thread;
 
 
 typedef struct {
-    char* address;
+    char *address;
     int filenumber;
-}writer_thread_info;
+} info_for_writer;
 
 typedef struct {
     int number_thread;
-}reader_thread_info;
+} info_for_reader;
 
 
-void* write_memory(void* data){
+void *write_memory(void *data) {
 
-    thread_info* cur_data = (thread_info*) data;
-    fread(cur_data->address, 1, cur_data->size, cur_data->file);
+    info_for_thread *current_data = (info_for_thread *) data;
+    fread(current_data->address, 1, current_data->size, current_data->file);
 
     pthread_exit(0);
 }
 
-void fill_space(char* address){
+void fill_memory(char *address) {
 
-    char* address_offset = address;
+    char *address_offset = address;
 
-    thread_info informations[D];
+    info_for_thread inf_for_thread[D];
     pthread_t threads[D];
 
     size_t size_for_thread = A * 1024 * 1024 / D;
-    FILE* filerandom = fopen("/dev/urandom", "rb");
+    FILE *urandom_file = fopen("/dev/urandom", "rb");
 
-    for (int i = 0; i < D; i ++){
-        informations[i].address = address_offset;
-        informations[i].size = size_for_thread;
-        informations[i].file = filerandom;
+    for (int i = 0; i < D; i++) {
+        inf_for_thread[i].address = address_offset;
+        inf_for_thread[i].size = size_for_thread;
+        inf_for_thread[i].file = urandom_file;
         address_offset += size_for_thread;
     }
 
-    informations[D - 1].size += A * 1024 * 1024 % D;
+    inf_for_thread[D - 1].size += A * 1024 * 1024 % D;
 
 
-    for (int i = 0; i < D; i ++)
-        pthread_create(&threads[i], NULL, write_memory, &informations[i]);
+    for (int i = 0; i < D; i++)
+        pthread_create(&threads[i], NULL, write_memory, &inf_for_thread[i]);
 
 
-    for (int i = 0; i < D; i ++)
+    for (int i = 0; i < D; i++)
         pthread_join(threads[i], NULL);
 
-    fclose(filerandom);
+    fclose(urandom_file);
 
 }
 
-void* generate_info(void* data){
-    char* cur_data = (char*) data;
-    while(loop == 1){
-        fill_space(cur_data);
+void *generate_info(void *data) {
+    char *current_data = (char *) data;
+    while (loop_flag == 1) {
+        fill_memory(current_data);
     }
-
     pthread_exit(0);
 }
 
-pthread_mutex_t get_mut(int id){
-    if (id == 0) return mut1;
-    if (id == 1) return mut2;
-    if (id == 2) return mut3;
+pthread_mutex_t get_mut(int id_file) {
+    if (id_file == 0) return mut1;
+    if (id_file == 1) return mut2;
+    if (id_file == 2) return mut3;
 }
 
 
-void write_file(writer_thread_info* data, int idFile){//todo можно не передавать айди, а брать из data->
+void write_memory_in_file(info_for_writer *data, int id_file) {
 
-    pthread_mutex_t cur_mut = get_mut(idFile);
-    pthread_mutex_lock(&cur_mut);
+    pthread_mutex_t current_mutex = get_mut(id_file);
+    pthread_mutex_lock(&current_mutex);
 
 
+    char *file_name = malloc(sizeof(char) * 6);
+    snprintf(file_name, sizeof(file_name) + 1, "labOS%d", ++id_file);
+    FILE *file = fopen(file_name, "wb");
 
-    char* defaultname = malloc(sizeof(char) * 6);
-    snprintf(defaultname, sizeof(defaultname) + 1, "labOS%d",++idFile);
-    FILE* file = fopen(defaultname, "wb");
 
-    //todo возможно переделать под полное заполнение файла областью памяти, хотя в задании сказано, что надо просто перенести память в файл, а не заполнять полностью её
     size_t memory_size = A * 1024 * 1024;
-    size_t rest = 0;
-    while (rest < memory_size) {
-        long  size = memory_size - rest >= G ? G : memory_size - rest;
-        rest += fwrite(data->address + rest, 1, size, file);
+    size_t wrote_count = 0;
+    while (wrote_count < memory_size) {
+        long will_write_count = memory_size - wrote_count >= G ? G : memory_size - wrote_count;
+        wrote_count += fwrite(data->address + wrote_count, 1, will_write_count, file);
     }
     fclose(file);
-    pthread_mutex_unlock(&cur_mut);
+    pthread_mutex_unlock(&current_mutex);
 
 }
 
-void* write_files(void* data){
-    writer_thread_info* cur_data = (writer_thread_info*) data;
-    while (loop == 1){
-        write_file(cur_data, cur_data->filenumber);
+void *write_files(void *data) {
+    info_for_writer *current_data = (info_for_writer *) data;
+    while (loop_flag == 1) {
+        write_memory_in_file(current_data, current_data->filenumber);
 
     }
 }
 
 
-void read_file(int id_thread, int idFile){
-    pthread_mutex_t cur_mut = get_mut(idFile);
-    pthread_mutex_lock(&cur_mut);
+void read_file(int id_thread, int id_file) {
+    pthread_mutex_t current_mutex = get_mut(id_file);
+    pthread_mutex_lock(&current_mutex);
 
 
-    char* defaultname = malloc(sizeof(char) * 6);
-    snprintf(defaultname, sizeof(defaultname) + 1, "labOS%d",++idFile);
-    FILE* file = fopen(defaultname, "rb");
+    char *file_name = malloc(sizeof(char) * 6);
+    snprintf(file_name, sizeof(file_name) + 1, "labOS%d", ++id_file);
+    FILE *file = fopen(file_name, "rb");
 
-    unsigned char block[G];
-    //todo переделать под подсчитывание среднего
-    fread(&block, 1 , G, file);
+    unsigned char block_for_read[G];
+    fread(&block_for_read, 1, G, file);
 
 
     unsigned int sum_in_block = 0;
-    unsigned int sum= 0;
+    unsigned int sum = 0;
     size_t parts = E * 1024 * 1024 / G;
 
-    for (size_t part = 0; part < parts; part ++){
+    for (size_t part = 0; part < parts; part++) {
 
-        for (size_t i = 0; i < sizeof(block); i += 4){
+        for (size_t i = 0; i < sizeof(block_for_read); i += 4) {
             unsigned int num = 0;
-            for (int j = 0; j < 4; j ++) {
-                num = (num<<8) + block[i + j];
+            for (int j = 0; j < 4; j++) {
+                num = (num << 8) + block_for_read[i + j];
             }
-            sum_in_block+=num;
+            sum_in_block += num;
         }
-        sum+=sum_in_block;
+        sum += sum_in_block;
     }
-    fprintf(stdout,"\nAvg from labOS%d from thread-%d is %lu\n",idFile, id_thread, sum/(parts*16) );
+    fprintf(stdout, "\nСреднее значение из файла labOS%d потока номер %d равно %lu\n", id_file, id_thread,
+            sum / (parts * 16));
     fflush(stdout);
     fclose(file);
-    pthread_mutex_unlock(&cur_mut);
+    pthread_mutex_unlock(&current_mutex);
 }
 
-void* read_files(void* data){
-    reader_thread_info* cur_data = (reader_thread_info*) data;
-    while (loop) {
-        for (int i = 0; i < filenumbers; i ++){
-            read_file(cur_data->number_thread, i);
+void *read_files(void *data) {
+    info_for_reader *current_data = (info_for_reader *) data;
+    while (loop_flag) {
+        for (int i = 0; i < number_of_files; i++) {
+            read_file(current_data->number_thread, i);
         }
     }
 }
 
 
+int main(void) {
+    printf("До аллокации");
+    getchar();
 
-int main(void){
-//    printf("Before allocation");
-//    getchar();
+    char *address = malloc(A * 1024 * 1024);
 
-    char* address = malloc(A*1024*1024);
+    printf("После аллокации");
+    getchar();
 
-//    printf("After allocation");
-//    getchar();
+    fill_memory(address);
 
-    fill_space(address);
-
-//    printf("After filling");
-//    getchar();
+    printf("После заполнения памяти");
+    getchar();
 
     free(address);
 
-//    printf("After deallocation");
-//    getchar();
+    printf("После деаллокации");
+    getchar();
 
     pthread_t generate_thread;
 
-    address = malloc(A*1024*1024);
+    address = malloc(A * 1024 * 1024);
 
     pthread_create(&generate_thread, NULL, generate_info, address);
     pthread_mutex_init(&mut1, NULL);
@@ -228,39 +224,35 @@ int main(void){
     pthread_mutex_init(&mut3, NULL);
 
 
-    pthread_t writer_thread[filenumbers];
-    writer_thread_info writer_information[filenumbers];
+    pthread_t writer_thread[number_of_files];
+    info_for_writer writer_information[number_of_files];
 
-    for (int  i = 0; i < filenumbers; i++) {
+    for (int i = 0; i < number_of_files; i++) {
         writer_information[i].filenumber = i;
         writer_information[i].address = address;
         pthread_create(&writer_thread[i], NULL, write_files, &writer_information[i]);
-
     }
 
     pthread_t reader_thread[I];
-    reader_thread_info reader_information[I];
+    info_for_reader reader_information[I];
 
-    for (int i = 0; i < I; i++){
+    for (int i = 0; i < I; i++) {
         reader_information[i].number_thread = i + 1;
         pthread_create(&reader_thread[i], NULL, read_files, &reader_information[i]);
     }
 
 
-
-    printf("Pause");
+    printf("Чтобы закончить бесконечный цикл нажмите ENTER два раза и дождитесь конца");
+    getchar();
     getchar();
 
-    printf("End");
-    getchar();
+    loop_flag = 0;
 
-    loop = 0;
-
-    for (int i = 0; i < I; i ++){
+    for (int i = 0; i < I; i++) {
         pthread_join(reader_thread[i], NULL);
     }
 
-    for (int i = 0; i < filenumbers; i ++)
+    for (int i = 0; i < number_of_files; i++)
         pthread_join(writer_thread[i], NULL);
 
     pthread_join(generate_thread, NULL);
@@ -269,10 +261,6 @@ int main(void){
     pthread_mutex_destroy(&mut2);
     pthread_mutex_destroy(&mut3);
 
-
     free(address);
     return 0;
 }
-//http://cppstudio.com/post/1641/ fread
-//http://cppstudio.com/post/1249/ fflush
-//https://learnc.info/c/pthreads_mutex_introduction.html mutex
