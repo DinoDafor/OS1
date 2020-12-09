79017330ae6e6f3da8f5ab361ac0ef3310f0d601
Mitin <dinodafor@mail.ru>
2020-12-09 17:14:42 +0300

    Begin of change

--- main.c
+++ main.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
+#include <sys/mman.h>
 #include <pthread.h>
-#include <malloc.h>
-//A=128;B=0x74594352;C=malloc;D=55;E=144;F=block;G=65;H=seq;I=48;J=avg;K=cv
-// ванс A=197;B=0xA35E1090;C=mmap;D=78;E=72;F=block;G=10;H=random;I=35;J=min;K=sem
-#define A 128
-#define B 0x74594352 //you can't do it with malloc
+#include <stdlib.h>
+#include <semaphore.h>
+/*
+ * Разработать программу на языке С, которая осуществляет следующие действия
 
-#define D 3
-#define E 144
-#define F block
-#define G 65
-#define H seq
-#define I 48
-#define J avg
-#define K cv
-/*Разработать программу на языке С, которая осуществляет следующие действия
-
-Создает область памяти размером A мегабайт, начинающихся с адреса B (если возможно) при помощи C=(malloc, mmap) заполненную случайными числами /dev/urandom в D потоков.
-Используя системные средства мониторинга определите адрес начала в адресном пространстве процесса и характеристики выделенных участков памяти. Замеры виртуальной/физической памяти необходимо снять:
+Создает область памяти размером A мегабайт, начинающихся с адреса B (если возможно) при помощи C=(malloc, mmap) заполненную случайными числами /dev/urandom в D потоков. Используя системные средства мониторинга определите адрес начала в адресном пространстве процесса и характеристики выделенных участков памяти. Замеры виртуальной/физической памяти необходимо снять:
 
 До аллокации
 После аллокации
@@ -43,85 +29,127 @@
 Отследить трассу системных вызовов.
 
 Используя stap построить графики системных характеристик.
-*/
-pthread_mutex_t mutex;
-const int filenumbers =1; //todo не подходит кажется
+ */
+//vancA=197;B=0xA35E1090;C=mmap;D=78;E=72;F=block;G=10;H=random;I=35;J=min;K=sem
+//mitin A=128;B=0x74594352;C=malloc;D=55;E=144;F=block;G=65;H=seq;I=48;J=avg;K=cv
+
+#define A 197
+#define B 0xA35E1090
+#define D 78
+#define E 72
+#define G 10
+#define I 35
+
+
+const int filenumbers = A/ E + (A % E == 0 ? 0: 1);
+sem_t sem1;
+sem_t sem2;
+sem_t sem3;
+//pthread_mutex_t mut;
+
 int loop = 1;
 
 typedef struct {
     char* address;
     size_t size;
     FILE* file;
-}thread_args;
+}thread_info;
+
+
 typedef struct {
     char* address;
     int filenumber;
 }writer_thread_info;
 
+typedef struct {
+    int number_thread;
+}reader_thread_info;
 
-void* writeInMemory(void* args) {
-   thread_args* current_data = (thread_args*) args;
-    fread(current_data->address, sizeof (char), current_data->size, current_data->file);
-pthread_exit(0);
+
+char* allocate(){
+    return mmap(B, A * 1024 * 1024,
+                PROT_EXEC | PROT_READ | PROT_WRITE,
+                MAP_PRIVATE | MAP_ANONYMOUS,
+                -1,
+                0);
 }
-void fill_memory(char* point_address){
 
-    thread_args  infForThreads[D];
-    pthread_t poolOfThreads[D];
+void* write_memory(void* data){
+
+    thread_info* cur_data = (thread_info*) data;
+    fread(cur_data->address, 1, cur_data->size, cur_data->file);
+
+    pthread_exit(0);
+}
+
+void fill_space(char* address){
+
+    char* address_offset = address;
+
+    thread_info informations[D];
+    pthread_t threads[D];
 
     size_t size_for_thread = A * 1024 * 1024 / D;
-    FILE* urandomFileStream = fopen("/dev/urandom", "rb"); //modes?
+    FILE* filerandom = fopen("/dev/urandom", "rb");
 
     for (int i = 0; i < D; i ++){
-        infForThreads[i].address = point_address;
-        infForThreads[i].size = size_for_thread;
-        infForThreads[i].file = urandomFileStream;
-        point_address += size_for_thread;
+        informations[i].address = address_offset;
+        informations[i].size = size_for_thread;
+        informations[i].file = filerandom;
+        address_offset += size_for_thread;
     }
-    infForThreads[D - 1].size += A * 1024 * 1024 % D;
 
-    for (int i = 0; i < D; i ++)
-        pthread_create(&poolOfThreads[i], NULL, writeInMemory, &infForThreads[i]);
+    informations[D - 1].size += A * 1024 * 1024 % D;
 
 
     for (int i = 0; i < D; i ++)
-        pthread_join(poolOfThreads[i], NULL);
+        pthread_create(&threads[i], NULL, write_memory, &informations[i]);
 
-    fclose(urandomFileStream); // проверить на 0 значение через if
-    //
 
+    for (int i = 0; i < D; i ++)
+        pthread_join(threads[i], NULL);
+
+    fclose(filerandom);
 
 }
+
 void* generate_info(void* data){
     char* cur_data = (char*) data;
     while(loop == 1){
-        fill_memory(cur_data);
+        fill_space(cur_data);
     }
 
     pthread_exit(0);
 }
 
-void write_file(writer_thread_info* data, int idFile){
-    pthread_mutex_lock(&mutex);
+sem_t* get_sem(int id){
+    if (id == 0) return &sem1;
+    if (id == 1) return &sem2;
+    if (id == 2) return &sem3;
+}
+
+
+void write_file(writer_thread_info* data, int idFile){//todo можно не передавать айди, а брать из data->
+    // pthread_mutex_lock(&mut);
+    sem_t* cur_sem = get_sem(idFile);
+    sem_wait(cur_sem);
 
     char* defaultname = malloc(sizeof(char) * 6);
     snprintf(defaultname, sizeof(defaultname) + 1, "labOS%d",++idFile);
     FILE* file = fopen(defaultname, "wb");
 
     size_t file_size = E * 1024 * 1024;
-    size_t wrote_count = 0;
-    //todo нейминг rest and size
-    while (wrote_count < file_size) {
-
-        long count_on_write = file_size - wrote_count >= G ? G : file_size - wrote_count;
-        wrote_count += fwrite(data->address + wrote_count, 1, count_on_write, file);
+    size_t rest = 0;
+    while (rest < file_size) {
+        long size = file_size - rest >= G ? G : file_size - rest;
+        rest += fwrite(data->address + rest, 1, size, file);
     }
-    pthread_mutex_unlock(&mutex);
-
+    //pthread_mutex_unlock(&mut);
+    sem_post(cur_sem);
 }
+
 void* write_files(void* data){
     writer_thread_info* cur_data = (writer_thread_info*) data;
-
     while (loop == 1){
         write_file(cur_data, cur_data->filenumber);
 
@@ -129,47 +157,139 @@ void* write_files(void* data){
 }
 
 
-int main() {
+void read_file(int id_thread, int idFile){
+    sem_t* cur_sem = get_sem(idFile);
+    sem_wait(cur_sem);
 
-//    char* address = malloc(A*1024*1024);
-//
-//
-//    fill_memory(address);
-//
-//
-//   free(address);
-//
-   pthread_t generate_thread;
 
-char* address = malloc(A*1024*1024);
-    //WRITE BEGIN
+    char* defaultname = malloc(sizeof(char) * 6);
+    snprintf(defaultname, sizeof(defaultname) + 1, "labOS%d",++idFile);
+    FILE* file = fopen(defaultname, "rb");
+
+    unsigned char block[G];
+    //todo переделать под подсчитывание среднего
+    fread(&block, 1 , G, file);
+
+    int min = 0;
+    int flag = 1;
+
+    size_t parts = E * 1024 * 1024 / G;
+
+    for (size_t part = 0; part < parts; part ++){
+        int min_from_block = 0;
+        int flag_for_block = 1;
+        for (size_t i = 0; i < sizeof(block); i += 4){
+            unsigned int num = 0;
+
+            for (int j = 0; j < 4; j ++) {
+                num = (num<<8) + block[i + j];
+            }
+            if (flag_for_block == 1){
+                flag_for_block = 0;
+                min_from_block = num;
+            }else{
+                if (min_from_block > num) min_from_block = num;
+            }
+        }
+        if (flag == 1) {
+            flag = 0;
+            min = min_from_block;
+        }else{
+            if (min > min_from_block) min = min_from_block;
+        }
+    }
+    fprintf(stdout,"\nMin from labOS%d from thread-%d is %d\n",idFile, id_thread, min );
+    fflush(stdout);
+    fclose(file);
+    sem_post(cur_sem);
+}
+
+void* read_files(void* data){
+    reader_thread_info* cur_data = (reader_thread_info*) data;
+    while (loop) {
+        for (int i = 0; i < filenumbers; i ++){
+            read_file(cur_data->number_thread, i);
+        }
+    }
+}
+
+
+
+int main(void){
+//    printf("Before allocation");
+//    getchar();
+
+    char* address = malloc(A*1024*1024);
+
+//    printf("After allocation");
+//    getchar();
+
+    fill_space(address);
+
+//    printf("After filling");
+//    getchar();
+
+    free(address);
+
+//    printf("After deallocation");
+//    getchar();
+
+    pthread_t generate_thread;
+
+    address = malloc(A*1024*1024);
+
     pthread_create(&generate_thread, NULL, generate_info, address);
+    // pthread_mutex_init(&mut, NULL);
+    sem_init(&sem1, 0, 1);
+    sem_init(&sem2, 0, 1);
+    sem_init(&sem3, 0, 1);
 
     pthread_t writer_thread[filenumbers];
-    pthread_mutex_init(&mutex, NULL);// todo инициализация мьютекса
     writer_thread_info writer_information[filenumbers];
+
     for (int  i = 0; i < filenumbers; i++) {
         writer_information[i].filenumber = i;
         writer_information[i].address = address;
+
     }
+    //todo почему не создавать в прошлом цикле?
     for (int i = 0; i < filenumbers; i ++)
         pthread_create(&writer_thread[i], NULL, write_files, &writer_information[i]);
-    //WRITE END
+
+    pthread_t reader_thread[I];
+    reader_thread_info reader_information[I];
+
+    for (int i = 0; i < I; i++){
+        reader_information[i].number_thread = i + 1;
+        pthread_create(&reader_thread[i], NULL, read_files, &reader_information[i]);
+    }
 
 
+
+    printf("Pause");
+    getchar();
+
+    printf("End");
+    getchar();
+
+    loop = 0;
+
+    for (int i = 0; i < I; i ++){
+        pthread_join(reader_thread[i], NULL);
+    }
+
+    for (int i = 0; i < filenumbers; i ++)
+        pthread_join(writer_thread[i], NULL);
+
+    pthread_join(generate_thread, NULL);
+    //pthread_mutex_destroy(&mut);
+    sem_destroy(&sem1);
+    sem_destroy(&sem2);
+    sem_destroy(&sem3);
+
+    free(address);
+    return 0;
 }
-
-//https://prog-cpp.ru/c-pointers/
-//https://ru.stackoverflow.com/questions/785269/Недопонимание-с-функцией-pthread-create
-//https://learnc.info/c/pthreads_create_and_join.html
 //http://cppstudio.com/post/1641/ fread
-//https://learnc.info/c/functions.html
-//https://learnc.info/c/structures.html
-//https://stackoverflow.com/questions/58087929/incompatible-pointer-types-passing-void-void-to-parameter-of-type-void
-//http://cppstudio.com/post/1245/
-// https://it.wikireading.ru/24955 mmap, munmap, msync;
-//https://prog-cpp.ru/c-files/
-//https://ru.stackoverflow.com/questions/1067483/Ошибка-process-finished-with-exit-code-139-interrupted-by-signal-11-sigsegv
-//http://uinc.ru/articles/34/ processes and threads
-//https://stackoverflow.com/questions/1345670/stack-smashing-detected
-//http://cppstudio.com/post/1637/ fwrite
+//http://cppstudio.com/post/1249/ fflush
+//
